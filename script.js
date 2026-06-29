3c8617038a7893a1452682ca85078b777b576d33
LYHips <158718122+LYHips@users.noreply.github.com>
2026-06-29 14:26:21 +0300

    pizza 1

---
+++ script.js
@@ -0,0 +1,81 @@
+function saveToLocalStorage(name, data) {
+    try {
+        localStorage.setItem(name, JSON.stringify(data));
+        return true;
+    } catch (error) {
+        console.error(`Failed to save "${name}" to LocalStorage:`, error);
+        return false;
+    }
+}
+
+function loadFromLocalStorage(name) {
+    try {
+        const data = localStorage.getItem(name);
+
+        if (data === null) {
+            return null;
+        }
+
+        return JSON.parse(data);
+    } catch (error) {
+        console.error(`Failed to load "${name}" from LocalStorage:`, error);
+        return null;
+    }
+}
+
+
+
+let my_cart = loadFromLocalStorage("cart")
+if (my_cart === null) my_cart = {}
+let count = 0;
+
+for(const [k, v] of Object.entries(my_cart))count += v
+
+document.getElementById('cart-count').innerText = count; 
+
+let products = []
+
+function addToCart(id) {
+
+    if (my_cart[id] == undefined) my_cart[id] = 0
+    my_cart[id] += 1; 
+    console.log (my_cart)
+    count += 1; 
+    document.getElementById('cart-count').innerText = count; 
+ 
+
+    saveToLocalStorage("cart", my_cart)
+
+}
+
+
+
+
+
+
+
+const card_con = document.querySelector(".pizza-grid")
+function render_card(product) {
+    return `<div class="pizza-card">
+                <img src="${product.image}" alt="${product.name}" class="pizza-img">
+                <div class="pizza-info">
+                    <h3 class="pizza-title">${product.name}</h3>
+                    <p class="pizza-desc">${product.desc}</p>
+                    <div class="pizza-footer">
+                        <span class="pizza-price">${product.price} ₴</span>
+                        <button class="btn-add" onclick="addToCart(${product.id})">Додати</button>
+                    </div>
+                </div>
+            </div>`
+
+}
+
+async function render_cat() {
+    const resp = await fetch("data.json")
+    products = await resp.json()
+    card_con.innerHTML = "" 
+    products.forEach((product) => card_con.innerHTML += render_card(product))
+}
+
+render_cat()
+
