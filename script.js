e9a821fc2c6f23f380fb06eedefcb9759812b25c
LYHips <158718122+LYHips@users.noreply.github.com>
2026-07-02 15:01:31 +0300

    p 1.2

--- script.js
+++ script.js
@@ -46,6 +46,10 @@ function addToCart(id) {
 
     saveToLocalStorage("cart", my_cart)
 
+
+render_cart()
+
+
 }
 
 
@@ -78,4 +82,84 @@ async function render_cat() {
 }
 
 render_cat()
+
+
+
+
+
+async function render_cart() {
+    const resp = await fetch("data.json")
+    products = await resp.json()
+    const cart_products = products.filter(p=>Object.keys(my_cart).includes(String(p.id)))
+    const cart_it = document.querySelector("#cart-items")
+
+    cart_it.innerHTML = ""
+
+    let total_pri = document.querySelector("#cart-total-amount")
+    let total_amount = 0;
+
+
+
+    cart_products.forEach(p=>{
+
+
+        total_amount += p.price * my_cart[p.id]
+
+        cart_it.innerHTML += `
+    <div class="cart-item">
+            <img class="cart-img" width = "60px" src=${p.image}>
+            <div class="cart-item-details">
+                <h4 class="cart-item-title">${p.name}</h4>
+                <div class="cart-item-meta">
+                    <span class="cart-item-qty">${my_cart[p.id]}  шт.</span>
+                    <span class="cart-item-price">${p.price*my_cart[p.id]}₴</span>
+                </div>
+            </div>
+        </div>
+    `
+     } )
+    
+    
+     total_pri.innerHTML = `${total_amount} ₴`
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function toggleCart() {
+    const cartModal = document.getElementById('cart-modal');
+    
+    
+    if (cartModal.style.display === 'flex') {
+        cartModal.style.display = 'none';
+    } else {
+        cartModal.style.display = 'flex';
+    }
+}
+
+
+function checkout() {
+    alert('Замовлення прийнято в обробку!');
+    localStorage.clear();
+    my_cart = {}
+    count = 0;
+    render_cart()
+    document.getElementById('cart-count').innerText = count; 
+}
+
+
+
+
+
+
+
+render_cart()
 
